cd514d9749d95d831e3aeaf60d3d580bca8d1297
Lks9172 <ekdan9172@gmail.com>
2021-09-05 19:00:50 +0900

    feat: 기본적인 라우팅 구성

--- srcs/app.ts
+++ srcs/app.ts
@@ -2,5 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
-app.listen(process.env.PORT, () => console.log(`server port ${process.env.PORT}`));
+import router from './routes';
+
+const runServer = async () => {
+
+    console.log('Set application...');
+    const app = express();
+
+    /* Set middleware */
+    app.use(router);
+
+    /* Run server */
+    app.listen(process.env.PORT, () => console.log(`server Run with port: ${process.env.PORT}`));
+};
+
+runServer();
