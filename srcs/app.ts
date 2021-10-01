f6285462733aed8c87a8e266e586f796f92eda91
hminn <hmkim0008@gmail.com>
2021-10-01 12:32:30 +0900

    [DON-31] feat: Express 서버 기본 세팅
    - 필요한 패키지 추가
    - lint 및 vscode, gitignore 설정 추가
    - app.ts : 서버 실행 코드 작성

--- srcs/app.ts
+++ srcs/app.ts
@@ -1,19 +1,39 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './routes';
+import cookieParser from 'cookie-parser';
+import sequelize from './models';
+
 dotenv.config();
 
-import router from './routes';
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+sequelize
+    .sync({ force: false })
+    .then(async () => {
+        console.log('Database connection success');
+    })
+    .catch((error) => console.log(error.message));
+
+app.use(router);
 
 const runServer = async () => {
-
-    console.log('Set application...');
-    const app = express();
-
-    /* Set middleware */
-    app.use(router);
-
     /* Run server */
-    app.listen(process.env.PORT, () => console.log(`server Run with port: ${process.env.PORT}`));
+    console.log('Set application...');
+    app.listen(process.env.PORT, async () => {
+        console.log(`server Run with port: ${process.env.PORT}`);
+        // await sequelize
+        //     .authenticate()
+        //     .then(async () => {
+        //         console.log('DB connection success');
+        //     })
+        //     .catch((error) => {
+        //     });
+    });
 };
 
 runServer();
